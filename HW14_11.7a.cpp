b2f1b4f1312b4aa2c33d9e2d38ea176ddfa0e4f0
kryvm <90252775+kryvm@users.noreply.github.com>
2021-12-01 12:04:20 +0200

    hW14 Kryv Compmath-1
    hW14 Kryv Compmath-1

---
+++ HW14_11.7a.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#define N 100
+
+int main() {
+    const char* fname="F.dat";
+    FILE* F=fopen(fname, "rb");
+    int mas[N];
+    int item;
+    int k=0;
+    while (!feof(F)) {
+        int res=fread(&item, sizeof(item),1, F);
+        if(res!=1) break;
+        mas[k++]=item;
+    }
+
+    int sum = 0;
+    
+    for (int i = 0; i < k; i++) {
+        sum+=mas[i];
+    }
+    printf("sum = %d",sum);
+    fclose(F);
+}
